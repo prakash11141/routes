003cdf3b8f93ef96dff1e2088377624af220bf18
Prakash <111prakashshrestha11141@gmail.com>
2024-08-26 11:08:27 +0545

    first commit

---
+++ data/point.js
@@ -0,0 +1,141 @@
+var pointJson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: { node: "A" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.31880649542126, 27.6830306863436],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "B" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.317255656366, 27.680515883090806],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "C" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.32015592680716, 27.68128281504282],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "D" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.32041775677772, 27.679249541802236],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "E" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.31640974415319, 27.67751944617146],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "F" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.31856480621849, 27.675807159597426],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "G" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.3212195453259, 27.6782244908958],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { node: "H" },
+      geometry: {
+        type: "Point",
+        coordinates: [85.31990317772369, 27.676940197970765],
+      },
+    },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.3212195453259, 27.67549782022273],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32068407375925, 27.67441108468161],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32508596686199, 27.6733743796284],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32234484110722, 27.67393190558535],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32435150254537, 27.67308400039944],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32352523018801, 27.67454750385764],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32504662055908, 27.674036441385482],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.3214398761454, 27.677427993078183],
+    //     type: "Point",
+    //   },
+    // },
+    // {
+    //   type: "Feature",
+    //   properties: {},
+    //   geometry: {
+    //     coordinates: [85.32132183723809, 27.676405892693367],
+    //     type: "Point",
+    //   },
+    // },
+  ],
+};
